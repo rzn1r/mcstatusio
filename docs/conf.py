1589afdb2ad6dfb5c16b4236789d7762e77ff497
Rezn1r <rezn1r@proton.me>
2025-10-29 11:31:15 +0800

    fix: change directory

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
 project = 'mcstatusio'
 copyright = '2025, Rezn1r'
 author = 'Rezn1r'
